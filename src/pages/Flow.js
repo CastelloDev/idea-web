309e40e35412ca3fbf5ddb046158590c0b725c2e
CastelloDev <castello.dev@gmail.com>
2022-08-13 11:25:29 +0200

    Made flow layout static on reload

--- src/pages/Flow.js
+++ src/pages/Flow.js
@@ -9,6 +9,7 @@ import { getAllIdeas } from "../services/IdeaService.js";
 import { useEffect, useState } from "react";
 import { createIdeaNode, getNodeEdges } from "../util/general.js";
 import nodeTypes from "../data/nodeTypes.js";
+import dagre from "dagre";
 
 const edgeOptions = {
   animated: true,
@@ -19,14 +20,55 @@ const edgeOptions = {
 
 const connectionLineStyle = { stroke: "black" };
 
+function getDagreNodeEdges(nodes, edges) {
+  const dagreGraph = new dagre.graphlib.Graph();
+  dagreGraph.setDefaultEdgeLabel(() => ({}));
+  dagreGraph.setGraph({ rankdir: "TB" });
+  const nodeWidth = 240;
+  const nodeHeight = 140;
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = "top";
+    node.sourcePosition = "bottom";
+
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+  });
+
+  return { nodes, edges };
+}
+
 function Flow() {
   const [nodes, setNodes] = useState(defaultNodes);
   const [edges, setEdges] = useState(defaultEdges);
 
   useEffect(() => {
     getAllIdeas().then((ideas) => {
-      setNodes(ideas.map((idea) => createIdeaNode(idea)));
-      setEdges(getNodeEdges(ideas));
+      const _nodes = ideas.map((idea) => createIdeaNode(idea));
+      const _edges = getNodeEdges(ideas);
+
+      // Without dagre
+      // setNodes(_nodes);
+      // setEdges(_edges);
+
+      const { nodes: dagreNodes, edges: dagreEdges } = getDagreNodeEdges(
+        _nodes,
+        _edges
+      );
+      setNodes(dagreNodes);
+      setEdges(dagreEdges);
     });
   }, []);
 
